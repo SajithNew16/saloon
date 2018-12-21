d1bec3a2e96a790a6819e8d422c2b3f59a92f512
SajithNew16 <sajithnew16@gmail.com>
2018-12-21 08:11:38 +0530

    registration ui

--- frontend/saloon/src/components/pages/stylistForm.js
+++ frontend/saloon/src/components/pages/stylistForm.js
@@ -28,40 +28,39 @@ class StylistForm extends Component {
 
     render() {
         return (
-            <form>
-                <h2>Stylist Registration Form</h2>
-                <div className="form-group row">
-                    <label className="col-5 col-md-4">User Name</label>
-                    <input type="text" className="form-control col-6 col-md-4" id="usernameStylist" placeholder="Enter user name" />
-                </div>
-                <div className="form-group">
-                    <label className="col-5 col-md-4">Email address</label>
-                    <input type="email" className="form-control col-6 col-md-4" id="emailStylist" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle col-5 col-md-4" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Experience
-                    </button>
-                    <div className="dropdown-menu col-6 col-md-4" aria-labelledby="dropdownMenuButton">
-                        <button className="dropdown-item" >1 year or less than 1 year</button>
-                        <button className="dropdown-item" >2 years or less than 2 years</button >
-                        <button className="dropdown-item" >3 years or less than 3 years</button >
-                        <button className="dropdown-item">more than 4 years</button >
+            <div>
+                <form>
+                    <h2>Stylist Registration Form</h2>
+                    <div className="form-group row">
+                        <label className="col-5 col-md-4">User Name</label>
+                        <input type="text" className="form-control col-6 col-md-4" id="usernameStylist" placeholder="Enter user name" />
                     </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-5 col-md-4">Password</label>
-                    <input type="password" className="form-control col-6 col-md-4" id="stylistPassword" placeholder="Password" />
-                </div>
-                <div className="form-group">
-                    <label className="col-5 col-md-4">Confirm Password</label>
-                    <input type="password" className="form-control col-6 col-md-4" id="stylistconfirmPassword" placeholder="Confirm Password" />
-                </div>
-                <button type="submit" className="btn btn-primary">Sign Up</button>
-            </form>
+                    <div className="form-group row">
+                        <label className="col-5 col-md-4">Email Address</label>
+                        <input type="email" className="form-control col-6 col-md-4" id="emailStylist" placeholder="Enter email address" />
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-5 col-md-4">Experience</label>
+                        <select className="form-control col-6 col-md-4 browser-default custom-select">
+                            <option value>Select years of experience</option>
+                            <option value="1">1 year or less than 1 year</option>
+                            <option value="2">2 years or less than 2 years</option>
+                            <option value="3">3 years or less than 3 years</option>
+                            <option value="4">more than 4 years</option>
+                        </select>
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-5 col-md-4">Password</label>
+                        <input type="password" className="form-control col-6 col-md-4" id="stylistPassword" placeholder="Password" />
+                    </div>
+                    <div className="form-group row">
+                        <label className="col-5 col-md-4">Confirm Password</label>
+                        <input type="password" className="form-control col-6 col-md-4" id="stylistconfirmPassword" placeholder="Confirm password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Sign Up</button>
+                </form>
+            </div>
         );
     }
 }
-
 export default StylistForm;
